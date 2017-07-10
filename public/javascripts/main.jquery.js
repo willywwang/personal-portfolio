e76a32686a9095e562a0a8e9ec655ab1d27339b6
Will <wang.yw.william@gmail.com>
2017-07-09 23:08:06 -0700

    Updating landing UX and client side controls

--- public/javascripts/main.jquery.js
+++ public/javascripts/main.jquery.js
@@ -4,5 +4,17 @@ function resizeHeader() {
 };
 
 $(document).ready(function(){
+	$(function(){
+		$("#landing-descriptors").typed({
+			strings: ['Computer Science Student', 'Web Development', 'Android Development', 'Colliegiate eSports', 'Customer Support', 'Computer Builder'],
+			typeSpeed: 50,
+			backSpeed: 25,
+			backDelay: 300,
+			loop: true,
+			loopCount: null,
+			cursorChar: null,
+		});
+	});
+
 	resizeHeader();
 });
