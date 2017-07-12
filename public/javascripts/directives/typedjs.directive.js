8982f99d4e489394a500c33fad6c52d2f311fe6e
Will <wang.yw.william@gmail.com>
2017-07-11 19:24:46 -0700

    moving files

--- public/javascripts/directives/typedjs.directive.js
+++ public/javascripts/directives/typedjs.directive.js
@@ -2,24 +2,20 @@ angular.module('websiteApp')
 .directive('typedjs', function() {
   return {
     restrict: 'E',
-
     scope: {
       strings: '='
     },
-
     template: '<span id="typed-output"></span>',
-
     link: function($scope, $element, $attrs) {
-
       var options = {
         strings: $scope.strings,
         contentType: "html",
-		typeSpeed: 50,
-		backSpeed: 25,
-		backDelay: 300,
-		loop: true,
-		loopCount: null,
-		cursorChar: null,
+        typeSpeed: 50,
+        backSpeed: 25,
+        backDelay: 300,
+        loop: true,
+        loopCount: null,
+        cursorChar: null,
       };
 
       $(function() {
